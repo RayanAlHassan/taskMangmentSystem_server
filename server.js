73930d7c9a22007908e8a0e91ea37672da89be20
Rayan <rayanalhassan2000@gmail.com>
2024-12-13 10:47:58 +0200

    deployments

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 
 const corsOption={
-    origin:['http://localhost:3000',"https://task-mangment-system-ten.vercel.app"],
+    origin:['http://localhost:3000',"https://task-mangment-system.vercel.app","https://task-mangment-system-client-two.vercel.app"],
     credentials:true,
     optionsSuccessStatus:200
 }
